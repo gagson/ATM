c87381c55ca878f350751f096763521b55d83713
gagson <70828758+gagson@users.noreply.github.com>
2021-04-06 21:51:07 +0900

    All ATM functions oked

--- atm_main.py
+++ atm_main.py
@@ -38,7 +38,7 @@ while True:
                         while True:
                             select = input(""
                                                 + "+-----------------------+\n"
-                                                + "|      command menu     |\n"
+                                                + "|      Command Menu     |\n"
                                                 + "+-----------------------+\n"
                                                 + "|1 --> Withdraw         |\n"
                                                 + "|2 --> Deposit          |\n"
@@ -67,6 +67,8 @@ while True:
                                     balance = cursor.execute("select balance from account where id = \'" + ac_id +"\'")
                                     for b in balance:
                                         print(b[0])
+                                    conn.execute("insert into transactions (account_id, amount, type) values (\'" + ac_id +"\', " + amount +", \'Withdraw\')")
+                                    conn.commit()                                    
                                 input("Press enter to continue.")
                             elif int(select) == 2:
                                 print("Amount to be deposited:")
@@ -82,6 +84,8 @@ while True:
                                     balance = cursor.execute("select balance from account where id = \'" + ac_id +"\'")
                                     for b in balance:
                                         print(b[0])
+                                conn.execute("insert into transactions (account_id, amount, type) values (\'" + ac_id +"\', " + amount +", \'Deposit\')")
+                                conn.commit()
                                 input("Press enter to continue.")
                             
                             elif int(select) == 3:
@@ -93,6 +97,8 @@ while True:
                                 amount = str(input(""))                          
                                 conn.execute("update account set balance = balance + " + amount + " where id = \'" + payee_id +"\'")
                                 conn.execute("update account set balance = balance - " + amount + " where id = \'" + ac_id +"\'")
+                                conn.execute("insert into transactions (account_id, amount, type) values (\'" + ac_id +"\', " + amount +", \'Transfer\')")
+                                conn.execute("insert into transactions (account_id, amount, type) values (\'" + payee_id +"\', " + amount +", \'Receive\')")
                                 conn.commit()
                                 print("Your Remaining Balance:")
                                 print("-----------------------")
@@ -106,12 +112,22 @@ while True:
                                 print("-------------")                            
                                 print(ac_balance)
                                 input("Press enter to continue.")
+
+                            elif int(select) == 5:
+                                print("Your Transaction Records:")
+                                print("-------------------------")
+                                cursor.execute("select date, type, amount from transactions where account_id = \'" + ac_id +"\'")
+                                print("(---------DATE--------, ---TYPE---, AMOUNT)")
+                                transactions_list = cursor.fetchall()                                
+                                for t in transactions_list:
+                                    print(t)
+                                input("Press enter to continue.")
                         break
                     if ac_type == "Admin":
                         while True:
                             select = input(""
                                                 + "+-----------------------+\n"
-                                                + "|   Admin command menu  |\n"
+                                                + "|   Admin Command Menu  |\n"
                                                 + "+-----------------------+\n"
                                                 + "|1 --> Add account      |\n"
                                                 + "|2 --> Delete account   |\n"
@@ -159,9 +175,18 @@ while True:
                                 ac_list = cursor.fetchall()
                                 print("Enter the account list filename (.json or .txt):")
                                 print("------------------------------------------------")
-                                filename = str(input(""))
-                                with open(filename, 'w') as f: 
+                                filename_ac = str(input(""))
+                                with open(filename_ac, 'w') as f: 
                                     json.dump(ac_list, f)
+                                    print("Success!")
+                                
+                                cursor.execute("select account_id, amount, type, date from transactions")
+                                transactions_list = cursor.fetchall()
+                                print("Enter the transactions list filename (.json or .txt):")
+                                print("-----------------------------------------------------")
+                                filename_transactions = str(input(""))
+                                with open(filename_transactions, 'w') as f: 
+                                    json.dump(transactions_list, f)
                                     print("Success!")
                                 input("Press enter to continue.")
                         break
